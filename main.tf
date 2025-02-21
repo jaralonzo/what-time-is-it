87a2b2667958de0be7cbdfa7bb9bc3f6efbec24b
Julie Ann Alonzo <julieannalonzo@mac.lan>
2025-02-21 15:40:30 +0100

    Add role

--- main.tf
+++ main.tf
@@ -13,9 +13,23 @@ module "bux-hw" {
       containers = {
         image = "europe-west4-docker.pkg.dev/bux-hw/bux-hw/what-time-is-it:latest"
       }
-      ports = {
-        container_port = 8080
-      }
     }
   }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
